b9347ce1ec2ea14e088436093c2707363218af82
lucaDaUbuntu <21833@studenti.marconiverona.edu.it>
2025-10-27 13:37:43 +0100

    made some progress with FIFO

--- TPI/C/FIFO (Named Pipes)/es3-1-1/consumatore.c
+++ TPI/C/FIFO (Named Pipes)/es3-1-1/consumatore.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -17,11 +18,46 @@ Scrivere due programmi C, produttore.c e consumatore.c.
     • Istruzioni per l’alunno: Eseguire i due programmi in due terminali separati
 */
 
+const int LIST_SIZE = 10;
+
 int main()
 {
     char myFifoPath[] = "/tmp/myfifo";
     mkfifo(myFifoPath, 0666);
-    int myFifo = open("/tmp/myfifo", O_WRONLY);
+    int myFifo = open("/tmp/myfifo", O_WRONLY, O_NONBLOCK);
+
+    char *listptr[LIST_SIZE];
+
+    // Writes the list of messages to send
+    for (int i = 0; i < LIST_SIZE; i++)
+    {
+        int length = snprintf(NULL, 0, "%d", i); // returns the lengh of the parsed string
+        const char suffix[] = " CIAO\n";
+        /* allocate space for the digits, the suffix and the terminating '\0' */
+        listptr[i] = malloc(length + sizeof(suffix));
+        if (listptr[i] == NULL)
+        {
+            perror("malloc");
+            /* free previously allocated pointers */
+            for (int j = 0; j < i; j++)
+                free(listptr[j]);
+            return 1;
+        }
+        /* build the final string */
+        snprintf(listptr[i], length + sizeof(suffix), "%d%s", i, suffix);
+    }
+
+    // Writes one message per second on the FIFO
+    for (int i = 0; i < LIST_SIZE; i++)
+    {
+        sleep(1);
+        write(myFifo, listptr[i], sizeof(listptr[i]));
+        printf("%d", (int)sizeof(listptr[i]));
+    }
+
+    /* free allocated memory */
+    for (int i = 0; i < LIST_SIZE; i++)
+        free(listptr[i]);
 
     return 0;
 }
