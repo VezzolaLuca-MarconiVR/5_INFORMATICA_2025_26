ade0d945b156ca77d83862a047339748cbaf11eb
lucaWindows <21833@studenti.marconiverona.edu.it>
2025-11-14 14:07:15 +0100

    stopped working on React - from now on, JS, HTML and CSS only!!!

--- INI/progetti/Ekhoikos_ROOT/project/js/index.js
+++ INI/progetti/Ekhoikos_ROOT/project/js/index.js
@@ -1,8 +1,1 @@
-const peelElements = document.querySelectorAll('.peel-text');
 
-peelElements.forEach(element => {
-  element.addEventListener('click', function () {
-    this.classList.toggle('peeled');
-    this.parentElement.classList.toggle('peeled');
-  });
-});
