7bb3ef89d58b3041f1cc28acf8983d35a2a5fc26
luca casa <21833@studenti.marconiverona.edu.it>
2025-11-24 20:46:37 +0100

    added testing and corrected

--- INI/progetti/javascript/esercizi/Lab 14_11 Javascript.js
+++ INI/progetti/javascript/esercizi/Lab 14_11 Javascript.js
@@ -1,5 +1,5 @@
 /*
-creare le funzioni lambda, con i commenti:
+Consegna: creare le funzioni lambda, con i commenti:
     1. somma, riceve un array e restituisce la somma degli elementi
     2. concatena, riceve un array di stringhe e le concatena 
     3. cerca, riceve una stringa e dice se sono presenti caratteri non dell'alfabeto
