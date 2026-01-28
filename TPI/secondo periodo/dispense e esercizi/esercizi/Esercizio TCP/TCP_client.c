522605adc1403541549e7b5a47fdbcbe09c185df
luca casa <21833@studenti.marconiverona.edu.it>
2026-01-28 17:06:07 +0100

    went on

--- TPI/secondo periodo/dispense e esercizi/esercizi/Esercizio TCP/TCP_client.c
+++ TPI/secondo periodo/dispense e esercizi/esercizi/Esercizio TCP/TCP_client.c
@@ -3,58 +3,150 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <stdio.h>
+#include <sys/types.h>
+
+#include "common_constants.h"
 
 // Link con ws2_32.lib
 #pragma comment(lib, "Ws2_32.lib")
 
+#ifndef ssize_t
+#if defined(_WIN64)
+typedef __int64 ssize_t;
+#else
+typedef long ssize_t;
+#endif
+#endif
+
 int main()
 {
-    WSADATA wsaData;
-    SOCKET ConnectSocket = INVALID_SOCKET;
-    struct sockaddr_in clientService;
-    char *sendbuf = "Hello from Winsock Client";
-    char recvbuf[512];
-    int iResult;
+  WSADATA wsaData;
+  SOCKET ConnectSocket = INVALID_SOCKET;
+  struct sockaddr_in clientService;
+  ssize_t bytesRead = 0;
+  boolean closeConnection = FALSE;
+  char sendbuf[GERERAL_BUFF_SIZE];
+  char recvbuf[GERERAL_BUFF_SIZE];
+  int iResult;
 
-    // Inizializzazione Winsock
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+  // Inizializzazione Winsock
+  if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+  {
+    printf("WSAStartup fallito\n");
+    return 1;
+  }
+
+  // Creazione socket
+  ConnectSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if (ConnectSocket == INVALID_SOCKET)
+  {
+    printf("Errore socket: %ld\n", WSAGetLastError());
+    WSACleanup();
+    return 1;
+  }
+
+  // Configurazione server (IP e Porta)
+  clientService.sin_family = AF_INET;
+  clientService.sin_addr.s_addr = inet_addr("127.0.0.1"); // IP Server
+  clientService.sin_port = htons(SERVER_PORT);            // Porta Server
+
+  // Connessione al server
+  if (connect(ConnectSocket, (SOCKADDR *)&clientService, sizeof(clientService)) == SOCKET_ERROR)
+  {
+    printf("Connessione fallita: %ld\n", WSAGetLastError());
+    closesocket(ConnectSocket);
+    WSACleanup();
+    return 1;
+  }
+
+  printf("Connesso al server.\n");
+
+  do
+  {
+    // Ask user to input string
+    bytesRead = my_getline(&sendbuf, sizeof(sendbuf), stdin);
+    if (bytesRead < 0)
     {
-        printf("WSAStartup fallito\n");
-        return 1;
+      // Getline error
+      perror("Error: my_getline input invalid");
     }
-
-    // Creazione socket
-    ConnectSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (ConnectSocket == INVALID_SOCKET)
+    else
     {
-        printf("Errore socket: %ld\n", WSAGetLastError());
-        WSACleanup();
-        return 1;
+      sendbuf[bytesRead - 1] = '\0';
+      bytesRead = 0;
     }
-
-    // Configurazione server (IP e Porta)
-    clientService.sin_family = AF_INET;
-    clientService.sin_addr.s_addr = inet_addr("127.0.0.1"); // IP Server
-    clientService.sin_port = htons(8888);                   // Porta Server
-
-    // Connessione al server
-    if (connect(ConnectSocket, (SOCKADDR *)&clientService, sizeof(clientService)) == SOCKET_ERROR)
-    {
-        printf("Connessione fallita: %ld\n", WSAGetLastError());
-        closesocket(ConnectSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    printf("Connesso al server.\n");
 
     // Invio dati
     iResult = send(ConnectSocket, sendbuf, (int)strlen(sendbuf), 0);
-    printf("Bytes inviati: %ld\n", iResult);
 
-    // Chiusura connessione
-    closesocket(ConnectSocket);
-    WSACleanup();
+    printf("Waiting for server response...");
 
-    return 0;
+    bytesRead = (int)recv(ConnectSocket, recvbuf, (int)strlen(recvbuf), 0);
+
+    recvbuf[bytesRead - 1] = '\0';
+
+    // CHECK FOR SERVER RESPONSE (IN CODES)
+  } while (closeConnection);
+
+  // Chiusura connessione
+  closesocket(ConnectSocket);
+  WSACleanup();
+
+  return 0;
+}
+
+ssize_t my_getline(char **lineptr, size_t *n, FILE *stream)
+{
+  if (lineptr == NULL || n == NULL || stream == NULL)
+  {
+    errno = EINVAL;
+    return -1;
+  }
+  size_t len = 0;
+  char *buffer = *lineptr;
+  size_t buffer_size = *n;
+  if (buffer == NULL || buffer_size == 0)
+  {
+    buffer_size = 128;
+    buffer = malloc(buffer_size);
+    if (buffer == NULL)
+    {
+      errno = ENOMEM;
+      return -1;
+    }
+    *lineptr = buffer;
+    *n = buffer_size;
+  }
+  int c;
+  while ((c = fgetc(stream)) != EOF)
+  { /* ensure room for char + terminating NUL */
+    if (len + 1 >= buffer_size)
+    { /* check for overflow */
+      if (buffer_size > SIZE_MAX / 2)
+      {
+        errno = ENOMEM;
+        return -1;
+      }
+      size_t new_size = buffer_size * 2;
+      char *new_buffer = realloc(buffer, new_size);
+      if (new_buffer == NULL)
+      {
+        errno = ENOMEM; /* keep original buffer intact; caller still owns it */
+        return -1;
+      }
+      buffer = new_buffer;
+      buffer_size = new_size;
+      *lineptr = buffer;
+      *n = buffer_size;
+    }
+    buffer[len++] = (char)c;
+    if (c == '\n')
+      break;
+  }
+  if (len == 0 && c == EOF)
+  {
+    return -1; /* no data read, EOF */
+  }
+  buffer[len] = '\0';
+  return (ssize_t)len;
 }
--- TPI/secondo periodo/dispense e esercizi/esercizi/Esercizio TCP/TCP_server.c
+++ TPI/secondo periodo/dispense e esercizi/esercizi/Esercizio TCP/TCP_server.c
@@ -4,75 +4,76 @@
 #include <winsock2.h>
 #include <windows.h>
 
+#include "common_constants.h"
+
 #pragma comment(lib, "ws2_32.lib") // Collega la libreria WinSock
 
-#define PORT 8888
 #define MAX_CLIENTS 10
 
 // Funzione gestore per ogni client (thread)
 DWORD WINAPI ClientHandler(LPVOID lpParam)
 {
-    SOCKET clientSocket = *(SOCKET *)lpParam;
-    char buffer[1024];
-    int bytesRead;
+  SOCKET clientSocket = *(SOCKET *)lpParam;
+  char buffer[GERERAL_BUFF_SIZE];
+  int bytesRead;
 
-    printf("Thread client creato.\n");
+  printf("Thread client creato.\n");
 
-    // Ricevi dati dal client
-    while ((bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0)) > 0)
-    {
-        buffer[bytesRead] = '\0';
-        printf("Client dice: %s", buffer);
-        send(clientSocket, buffer, bytesRead, 0); // Eco al client
-    }
+  // Ricevi dati dal client
+  while ((bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0)) > 0)
+  {
+    buffer[bytesRead] = '\0';
+    printf("Client dice: %s", buffer);
+    send(clientSocket, buffer, bytesRead, 0); // Eco al client
+  }
 
-    printf("Client disconnesso.\n");
-    closesocket(clientSocket);
-    return 0;
+  printf("Client disconnesso.\n");
+  closesocket(clientSocket);
+  return 0;
 }
 
 int main()
 {
-    WSADATA wsa;
-    SOCKET serverSocket, clientSocket;
-    struct sockaddr_in serverAddr, clientAddr;
-    int clientAddrLen = sizeof(clientAddr);
+  WSADATA wsa;
+  SOCKET serverSocket, clientSocket;
+  struct sockaddr_in serverAddr, clientAddr;
+  int clientAddrLen = sizeof(clientAddr);
 
-    // Inizializza WinSock
-    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
-    {
-        printf("Fallito. Codice errore: %d", WSAGetLastError());
-        return 1;
-    }
+  // Inizializza WinSock
+  if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+  {
+    printf("Fallito. Codice errore: %d", WSAGetLastError());
+    return 1;
+  }
 
-    // Crea socket
-    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+  // Crea socket
+  serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
-    serverAddr.sin_port = htons(PORT);
+  serverAddr.sin_family = AF_INET;
+  serverAddr.sin_addr.s_addr = INADDR_ANY;
+  serverAddr.sin_port = htons(SERVER_PORT);
 
-    // Bind
-    if (bind(serverSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
-    {
-        printf("Bind fallito.");
-        return 1;
-    }
+  // Bind
+  if (bind(serverSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+  {
+    printf("Bind fallito.");
+    return 1;
+  }
 
-    // Listen
-    listen(serverSocket, MAX_CLIENTS);
-    printf("Server in ascolto sulla porta %d...\n", PORT);
+  // Listen
+  listen(serverSocket, MAX_CLIENTS);
+  printf("Server in ascolto sulla porta %d...\n", SERVER_PORT);
 
-    while (1)
-    {
-        clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddr, &clientAddrLen);
-        printf("Nuova connessione accettata.\n");
+  while (1)
+  {
+    clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddr, &clientAddrLen);
+    printf("Nuova connessione accettata.\n");
 
-        // Crea un thread per gestire il client
-        CreateThread(NULL, 0, ClientHandler, &clientSocket, 0, NULL);
-    }
+    // Crea un thread per gestire il client
+    CreateThread(NULL, 0, ClientHandler, &clientSocket, 0, NULL);
+  }
 
-    closesocket(serverSocket);
-    WSACleanup();
-    return 0;
+  closesocket(serverSocket);
+  WSACleanup();
+  return 0;
 }
